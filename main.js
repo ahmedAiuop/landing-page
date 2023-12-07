581d977c3b78f59b5e8a630df825ea1c89871155
ahmedAiuop <ahmedaioup111@gmail.com>
2023-12-07 21:10:44 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let ahmed = document.querySelector('.ahmed');
+let tarek = document.getElementById('tarek');
+
+
+function phones(phone) {
+    ahmed.src = phone;
+}
+
+function colors(color) {
+    tarek.style.backgroundColor = color;
+}
